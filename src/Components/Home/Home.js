98aad38d0353002664997aa06f522bcf653b5a01
Rajan kumar <krcpr007@gmail.com>
2022-09-20 12:32:07 +0530

    skelton

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,8 +3,13 @@ import React from 'react'
 function Home() {
     return (
         <>
-            <div className="py-32 text-center">
-                <h2 className="font-extrabold text-4xl">IEEE SB NIT PATNA</h2>
+            <div className='flex bg-slate-900 '>
+                <div className='text-yellow-300 mt-40 w-full'>
+                    <h1 className='text-5xl m-2 font-medium'>IEEE is the world's largest technical professional organization dedicated to advancing technology for the benefit of humanity</h1>
+                </div>
+                <div className='mt-32'>
+                    <img src="settings.png" alt="" />
+                </div>
             </div>
 
         </>
