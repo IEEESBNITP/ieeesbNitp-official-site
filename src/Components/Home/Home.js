87e5aba5969f9ccaa9e79c1c24d4392d663eecbd
Rajan kumar <krcpr007@gmail.com>
2022-09-21 20:50:30 +0530

    some skelton made

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,27 @@
 import React from 'react'
-
+import { BsArrowRightCircleFill } from 'react-icons/bs'
 function Home() {
     return (
-        <>
-            <div className='flex bg-slate-900 '>
-                <div className='text-yellow-300 mt-40 w-full'>
-                    <h1 className='text-5xl m-2 font-medium'>IEEE is the world's largest technical professional organization dedicated to advancing technology for the benefit of humanity</h1>
-                </div>
-                <div className='mt-32'>
-                    <img src="settings.png" alt="" />
+        <>  <div className='bg-[#181F2A] '>
+            <div className=''>
+
+                <div className='grid h-screen place-items-center'>
+                    <img src="settings.png" className='w-96' alt="" />
                 </div>
             </div>
+            <div className='flex'>
+                <div>
+                    <img src="illustration-stay-productive.png" className='w-3/4 px-14 py-10 md:ml-10' alt="" />
+                </div>
+                <div className='w-3/5'>
+                    <div className='py-20'>
 
+                        <h1 className='text-4xl  font-medium text-left text-gray-300'>IEEE is the world's largest technical professional organization dedicated to advancing technology for the benefit of humanity</h1>
+                        <p className='flex font-serif font-medium underline text-[#5DD8D3] hover:text-gray-300 cursor-pointer'>Know More <BsArrowRightCircleFill className='m-1' /> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
