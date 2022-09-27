3e7d1c066b9b878104fe745566097e546ac4396c
Rajan kumar <krcpr007@gmail.com>
2022-09-27 18:58:08 +0530

    committe comp.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import AboutUs from "./pages/About/AboutUs";
 import Blogs from "./pages/Blogs/Blogs";
 import Membership from './pages/Membership/Membership'
 import Events from './pages/Events/Events'
+import Footer from "./Components/Footer/Footer";
+import Committee from "./pages/committee/Committee";
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/committee" element={<Committee/>} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/events" element={<Events />} />
         <Route path="/membership" element={<Membership />} />
       </Routes>
+      <Footer/>
     </>
   );
 }
