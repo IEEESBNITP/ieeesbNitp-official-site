53338afb88b34b417dcb74872419f854296df1a8
Rajan kumar <krcpr007@gmail.com>
2022-10-07 02:01:31 +0530

    latest 3 events

--- src/Components/Home/Updates.js
+++ src/Components/Home/Updates.js
@@ -1,63 +1,92 @@
-import React, {useEffect} from 'react'
-import {auth} from '../../Firebase'
+import React, { useEffect, useState } from 'react'
+import { db } from '../../Firebase'
+import {
+    collection,
+    getDocs,
+    query,
+    orderBy,
+    limit,
+} from 'firebase/firestore'
 function Updates() {
-   useEffect(()=>{
-         
-   },[])
+    const [loading, setLoading] = useState(false);
+    const [events, setEvents] = useState([]);
+    const fetchEvents = async () => {
+        try {
+            // Get reference
+            setLoading(true)
+            const eventRef = collection(db, 'events')
+
+            // Create a query
+            const q = query(
+                eventRef,
+
+                orderBy('date', 'desc'),
+                limit(3)
+            )
+            // Execute query
+            const querySnap = await getDocs(q)
+
+            // const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+            const event = []
+
+            querySnap.forEach((doc) => {
+                return event.push({
+                    id: doc.id,
+                    data: doc.data(),
+                })
+            })
+            setEvents(event);
+            // setListings(listings)
+            setLoading(false)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        fetchEvents();
+    }, [])
+    if(loading){
+        return <h1>Loading</h1>
+    }
     return (
-        <section className="pb-20 dark:bg-[#181F2A]  mt-2">
-            <div className="container mx-auto px-4">
+        <section className="pb-20 dark:bg-[#181F2A]  mt-5">
+            <div className="container mx-auto px-10">
                 <div className="flex flex-wrap">
-                    <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
+                    <div className="lg:pt-12 pt-6 w-full md:w-4/12 md:px-10  text-center">
                         <div
-                            className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg"
+                            className="relative flex flex-col min-w-0 break-words bg-white dark:bg-gray-900 shadow-2xl w-full mb-8 rounded-lg"
                         >
-                            <div className="px-4 py-5 flex-auto">
-                                <div
-                                    className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400"
-                                >
-                                    <i className="fas fa-award"></i>
-                                </div>
-                                <h6 className="text-xl font-semibold">Awarded Agency</h6>
-                                <p className="mt-2 mb-4 text-gray-600">
-                                    Divide details about your product or agency work into parts.
-                                    A paragraph describing a feature will be enough.
+                            <div className="flex-auto">
+                                <img src={events[0]?.data?.imgPath} alt="" />
+                                <h6 className="text-xl font-semibold">{events[0]?.data?.name}</h6>
+                                <p className="mt-2 mb-4 text-gray-400">
+                                {events[0]?.data?.desc.slice(0,105)+"..."}
                                 </p>
                             </div>
                         </div>
                     </div>
-                    <div className="w-full md:w-4/12 px-4 text-center">
+                    <div className="w-full md:w-4/12 md:px-10 text-center">
                         <div
-                            className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg"
+                            className="relative flex flex-col min-w-0 break-words bg-white dark:bg-gray-900 w-full mb-8 shadow-2xl rounded-lg"
                         >
-                            <div className="px-4 py-5 flex-auto">
-                                <div
-                                    className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-blue-400"
-                                >
-                                    <i className="fas fa-retweet"></i>
-                                </div>
-                                <h6 className="text-xl font-semibold">Free Revisions</h6>
-                                <p className="mt-2 mb-4 text-gray-600">
-                                    Keep you user engaged by providing meaningful information.
-                                    Remember that by this time, the user is curious.
+                            <div className=" flex-auto">
+                                <img src={events[1]?.data?.imgPath} alt="" />
+                                <h6 className="text-xl font-semibold">{events[1]?.data?.name}</h6>
+                                <p className="mt-2 mb-4 text-gray-400">
+                                   {events[1]?.data?.desc.slice(0,105)+"..."}
                                 </p>
                             </div>
                         </div>
                     </div>
-                    <div className="pt-6 w-full md:w-4/12 px-4 text-center">
+                    <div className="pt-6 w-full md:w-4/12 md:px-10 text-center">
                         <div
-                            className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg"
+                            className="relative flex flex-col min-w-0 break-words bg-white dark:bg-gray-900 w-full mb-8 shadow-2xl rounded-lg"
                         >
-                            <div className="px-4 py-5 flex-auto">
-                                <div
-                                    className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400"
-                                >
-                                    <i className="fas fa-fingerprint"></i>
-                                </div>
-                                <h6 className="text-xl font-semibold">Verified Company</h6>
-                                <p className="mt-2 mb-4 text-gray-600">
-                                    Write a few lines about each one. A paragraph describing a
-                                    feature will be enough. Keep you user engaged!
+                            <div className="flex-auto">
+                                <img src={events[2]?.data?.imgPath} alt="" />
+                                <h6 className="text-xl font-semibold">{events[2]?.data?.name}</h6>
+                                <p className="mt-2 mb-4 text-gray-400">
+                                {events[2]?.data?.desc.slice(0,105)+"..."}
                                 </p>
                             </div>
                         </div>
