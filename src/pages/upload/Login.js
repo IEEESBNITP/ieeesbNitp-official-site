a5a7a7060c00656921c94d93eca4e013ae26f6d1
Rajan kumar <krcpr007@gmail.com>
2022-10-07 17:14:18 +0530

    more authentication added

--- src/pages/upload/Login.js
+++ src/pages/upload/Login.js
@@ -1,20 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { signInWithEmailAndPassword } from "firebase/auth";
-import {auth} from '../../Firebase'
-import  { useNavigate } from 'react-router-dom'
+import { auth } from '../../Firebase'
+import { useNavigate } from 'react-router-dom'
 function Login() {
-   const navigate = useNavigate();
+    const navigate = useNavigate();
+    const [loader, setLoader] = useState(false);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const handleLogin = () => {
-        signInWithEmailAndPassword(auth, email, password)
+    const localAuth = JSON.parse(localStorage.getItem('ieee-auth'));
+    useEffect(() => {
+        if (auth && localAuth) {
+            navigate('/dashboard')
+            // if (auth.currentUser.uid === localAuth.uid) {
+            // }
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+    const handleLogin = (e) => {
+        e.preventDefault()
+        if(email !=='' && password!==''){
+
+            setLoader(true);
+            signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 // Signed in 
                 const user = userCredential.user;
-                // ...
-                console.log(user); 
-                localStorage.setItem('ieee-auth',JSON.stringify(user))
-                if(user){
+                localStorage.setItem('ieee-auth', JSON.stringify(user))
+                setLoader(false);
+                if (user) {
+                    setEmail(''); 
+                    setPassword('');
                     navigate('/dashboard')
                 }
             })
@@ -23,7 +38,11 @@ function Login() {
                 console.log(errorCode);
                 const errorMessage = error.message;
                 console.log(errorMessage);
+                setLoader(false);
             });
+        }else{
+            alert("Provide correct Inputs")
+        }
     }
 
     return (
@@ -38,21 +57,23 @@ function Login() {
                             <div>
                                 <h1 className="text-2xl font-semibold">IEEE SB NITP ADMIN LOGIN</h1>
                             </div>
-                            <div className="divide-y divide-gray-200">
-                                <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                                    <div className="relative">
-                                        <input autoComplete="off" id="email" name="email" onChange={e => setEmail(e.target.value)} type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
-                                        <label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
-                                    </div>
-                                    <div className="relative">
-                                        <input autoComplete="off" id="password" name="password" type="password" onChange={e => setPassword(e.target.value)} className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
-                                        <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
-                                    </div>
-                                    <div className="relative">
-                                        <button className="bg-blue-500 text-white rounded-md px-2 py-1" onClick={handleLogin}>Submit</button>
+                            <form>
+                                <div className="divide-y divide-gray-200">
+                                    <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                                        <div className="relative">
+                                            <input autoComplete="off" id="email" name="email" onChange={e => setEmail(e.target.value)} type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
+                                            <label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
+                                        </div>
+                                        <div className="relative">
+                                            <input autoComplete="off" id="password" name="password" type="password" onChange={e => setPassword(e.target.value)} className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
+                                            <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                                        </div>
+                                        <div className="relative">
+                                            <button className="bg-blue-500 text-white rounded-md px-2 py-1" type='submit' onClick={handleLogin}>{loader ? "Wait.." : "Login"}</button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
