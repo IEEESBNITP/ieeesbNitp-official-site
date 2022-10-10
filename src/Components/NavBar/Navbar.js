980e57b97ead736503b541b2c8d9e56be434109f
Rajan kumar <krcpr007@gmail.com>
2022-10-10 16:04:05 +0530

    btn style now good

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -45,9 +45,7 @@ function Navbar() {
                             <Link className="md:p-4 py-2 block hover:text-amber-600" to="/about">About</Link>
                         </li>
                         <li>
-                            {(localAuth && auth) ? <Link className="block py-2 pr-4 pl-3 hover:bg-gray-50 md:hover:bg-transparent md:border-0  md:p-0 dark:text-gray-400 md:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/dashboard"> <span className='md:text-amber-500 font-serif md:bg-amber-600 md:bg-opacity-20 md:border border-amber-500 md:hover:bg-opacity-10 md:p-3 rounded'>DashBoard</span>
-                            </Link> : <Link className="block py-2 pr-4 pl-3 hover:bg-gray-50 md:hover:bg-transparent md:border-0  md:p-0 dark:text-gray-400 md:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/login"> <span className='md:text-amber-500 font-serif md:bg-amber-600 md:bg-opacity-20 md:border border-amber-500 md:hover:bg-opacity-10 md:p-3 rounded'>Admin</span>
-                            </Link>}
+                            {(localAuth && auth) ? (<Link className="md:p-3 mt-1 block hover:text-amber-600 py-3 pr-4 md:pl-3  md:hover:bg-transparent md:border dark:text-amber-500 md:hover:text-amber-500 dark:hover:text-amber md:dark:hover:bg-transparent md:text-amber-500 font-serif md:bg-amber-600 md:bg-opacity-20 border-amber-500 md:hover:bg-opacity-10  rounded" to="/dashboard">Dashboard</Link>) : (<Link className="md:p-3 mt-1 block hover:text-amber-600 py-3 pr-4 md:pl-3  md:hover:bg-transparent md:border dark:text-amber-500 md:hover:text-amber-500 dark:hover:text-amber md:dark:hover:bg-transparent md:text-amber-500 font-serif md:bg-amber-600 md:bg-opacity-20 border-amber-500 md:hover:bg-opacity-10  rounded" to="/login">Admin</Link>)}
                         </li>
                     </ul>
                 </div>
