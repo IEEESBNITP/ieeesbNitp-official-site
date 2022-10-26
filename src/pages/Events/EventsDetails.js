ca777f3c99c312e95eb6df85eee370be1718875b
Rajan kumar <krcpr007@gmail.com>
2022-10-26 19:17:18 +0530

    Event details page

--- src/pages/Events/EventsDetails.js
+++ src/pages/Events/EventsDetails.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { doc, getDoc } from 'firebase/firestore'
+import SimpleLoader from '../PageLoader/SimpleLoader'
 import { db } from '../../Firebase'
 import { useParams } from 'react-router-dom'
 function EventsDetails() {
@@ -29,29 +30,29 @@ function EventsDetails() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  // function to detect is any hyperlink is there in string/text description 
-  const linkIfy = (text) => {
-    let urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    return text?.replace(urlRegex, function (url) {
-      return '<a style="color:#70B5F9;text-decoration:underline" className="hover:underline" href="' + url + '">' + url + '</a>';
-    });
-  }
   if (loader) {
-    return <h1 className='text-center text-3xl text-rose-600'>Loading...</h1>
+    return <SimpleLoader />
   }
   return (
     <>
-
       <div className="relative bg-white dark:bg-gray-800 p-4">
         <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-12 lg:items-center">
           <div className="lg:col-start-2 md:pl-20">
-            <h4 className="text-3xl leading-8 font-extrabold underline text-gray-900 dark:text-white tracking-tight sm:leading-9">
+            <h4 className="text-3xl leading-8 font-bold underline text-amber-700 dark:text-amber-600 tracking-tight sm:leading-9">
               {eventData?.name}
             </h4>
             <div>
-              {<p dangerouslySetInnerHTML={{ __html: linkIfy(eventData?.desc) }} />}
-              {/* {lorem} */}
+              <p className='text-sm font-medium'>{eventData?.desc?.split(new RegExp('\r?\n', 'g')).map(function (item, idx) {
+                // it replace the enter key or \n  with <br/> tag.
+                return (
+                  <span key={idx}>
+                    {item}
+                    <br />
+                  </span>
+                )
+              })}</p>
             </div>
+            <span className='text-amber-700'>Date : {eventData?.date}</span>
           </div>
           <div className="mt-10 -mx-4 md:-mx-12 relative lg:mt-0 lg:col-start-1">
             <img src={eventData?.imgPath} alt="illustration" className="relative mx-auto shadow-lg lg:w-[32rem] rounded w-auto" />
