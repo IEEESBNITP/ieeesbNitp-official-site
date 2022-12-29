5810169b60c8c4407a006e2647843578bdf305de
Rajan kumar <krcpr007@gmail.com>
2022-12-29 20:53:44 +0530

    gallery

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -1,36 +1,37 @@
-import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+import { collection, getDocs, orderBy, query, } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
-import { db } from '../../Firebase';
+import { Link } from 'react-router-dom';
+import { db, auth } from '../../Firebase';
 import SimpleLoader from '../PageLoader/SimpleLoader'
 import ImageCard from './ImageCard'
 function Gallery() {
+  const currentYear = new Date().getFullYear();
+  const [year, setYear] = useState(currentYear)
   const [data, setData] = useState([]);
   const [loader, setLoading] = useState(false);
   const fetchImages = async () => {
     try {
       // Get reference
       setLoading(true)
-      const eventRef = collection(db, 'gallery')
-
+      const galleryRef = collection(db, `gallery${parseInt(year)}`)
       // Create a query
       const q = query(
-        eventRef,
-        orderBy('year', 'desc')
+        galleryRef,
+        orderBy('timestamp', 'desc'),
       )
       // Execute query
       const querySnap = await getDocs(q)
 
-      // const lastVisible = querySnap.docs[querySnap.docs.length - 1]
       const data = [] // not a global variable 
 
       querySnap.forEach((doc) => {
-        return data.push({
-          id: doc.id,
-          data: doc.data(),
+        //running two time loops one for docs and second one for imgUrls array
+        doc.data()?.imgUrls?.forEach((doc) => {
+          return data.push(doc)
         })
+        // data.push(doc.data().imgUrls)
       })
-      setData(data[0]?.data?.imgUrls);
-      console.log(data)
+      setData(data);
       setLoading(false)
     } catch (error) {
       setLoading(false);
@@ -39,13 +40,31 @@ function Gallery() {
   }
   useEffect(() => {
     fetchImages();
-  }, [])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [year])
   if (loader) {
     return <SimpleLoader />
   }
+  const localAuth = JSON.parse(localStorage.getItem('ieee-auth'));
   return (
     <>
       <div>
+        <div className='flex justify-between'>
+          <div className='p-5'>
+            <label htmlFor="cars" className='text-gray-400'>Year </label>
+            <select name="year" id="year" className='bg-amber-600 rounded px-5 py-1 text-white' value={year} onChange={e => setYear(e.target.value)}>
+              <option value="2022">2022</option>
+              <option value="2021">2021</option>
+              <option value="2020">2020</option>
+              <option value="2019">2019</option>
+            </select>
+          </div>
+          {(auth?.currentUser && localAuth) ? <>
+            <div className='p-5'>
+              <Link to="/upload-gallery"><button className='bg-amber-600 rounded px-5 py-1 text-white'>Upload Pics</button></Link>
+            </div>
+          </> : null}
+        </div>
         <div className="p-4 md:p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
           {data?.map((item, id) => {
             return <ImageCard key={id} data={item} />
